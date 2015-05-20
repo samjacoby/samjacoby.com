569c3d3cd55091a4a95ec445a912dafa64c00b4e
Sam Jacoby <sam@formlabs.com>
2015-05-20 17:09:40 -0500

    Updating fabfile to keep track of latest changes.

--- site/fabfile.py
+++ site/fabfile.py
@@ -1,5 +1,6 @@
 from fabric.api import *
 import os
+from fabric.context_managers import cd
 import fabric.contrib.project as project
 
 PROD = 'shackman@shackmanpress.com'
@@ -8,6 +9,7 @@ KEY = '/homes/sjacoby/.ssh/id_bardamu'
 SITE_CONFIG = 'site.yaml'
 PROD_CONFIG = 'prod.yaml'
 
+LOCAL_PATH = '/Users/sjacoby/Code/Shackman/samjacoby.com/site'
 DEST_PATH = '/home/shackman/public_html/samjacoby.com/'
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 DEPLOY_PATH = os.path.join(ROOT_PATH, 'deploy')
@@ -16,42 +18,50 @@ PROD_AWS = 'sjacoby@yarbo.org'
 DEST_PATH_AWS= '/home/sjacoby/public_html/samjacoby.com/'
 
 def clean():
-    local('rm -rf ./deploy')
+    with cd(LOCAL_PATH):
+        local('rm -rf ./deploy')
 
 def gen(config=SITE_CONFIG):
-    generate(config)
+    with cd(LOCAL_PATH):
+        generate(config)
 
 def watch(config=SITE_CONFIG):
-    local('fswatch content "fab gen" &')
-    serve()
+    with cd(LOCAL_PATH):
+        local('fswatch content "fab gen" &')
+        serve()
 
 def generate(config=SITE_CONFIG):
-    local('hyde gen -c %s' % config)
+    with cd(LOCAL_PATH):
+        local('hyde gen -c %s' % config)
 
 def regen(config=SITE_CONFIG):
-    clean()
-    generate(config)
+    with cd(LOCAL_PATH):
+        clean()
+        generate(config)
 
 def serve():
-    local('hyde serve')
+    with cd(LOCAL_PATH):
+        local('hyde serve')
 
 def reserve():
-    regen()
-    serve()
+    with cd(LOCAL_PATH):
+        regen()
+        serve()
 
 def smush():
     local('smusher ./media/images')
 
 @hosts(PROD)
 def publish():
-    local('git push')
-    regen(PROD_CONFIG)
-    project.rsync_project(
-        remote_dir=DEST_PATH,
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True
-    )
-    run('ln -s ~/public_html/static ~/public_html/samjacoby.com/')
+    with cd(LOCAL_PATH):
+        local('git push')
+        regen(PROD_CONFIG)
+        project.rsync_project(
+            remote_dir=DEST_PATH,
+            local_dir=DEPLOY_PATH.rstrip('/') + '/',
+            delete=True
+        )
+        run('ln -s ~/public_html/static ~/public_html/samjacoby.com/')
 
 @hosts(PROD_AWS)
 def publish_aws():
